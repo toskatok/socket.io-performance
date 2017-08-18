c14a06902efd5a8d47cb7e2e752ec560dfee656c
Parham Alvani <parham.alvani@gmail.com>
2017-08-18 11:43:35 +0430

    Socket io and http server initiation was added.

---
+++ index.js
@@ -0,0 +1,40 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 18-08-2017
+ * |
+ * | File Name:     index.js
+ * +===============================================
+ */
+const app = require('http').createServer(handler);
+const io = require('socket.io')(app);
+const fs = require('fs')
+
+app.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('my message', function(msg){
+    console.log('my message: ' + msg);
+    io.emit('my message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
